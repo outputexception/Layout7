10d1fc1bae94edbf3a3503ebdeffd994e638166f
outputexception <outputexception@gmail.comm>
2016-06-25 15:25:39 +0200

    [F]nitial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/tpietras/pen/gMgayd.
+
+ from: http://tympanus.net/Tutorials/SmoothTransitionsResponsiveLayout/
---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+   $('#st-panel-4 .st-deco').click( function(e){
+   $('#st-panel-4 .st-deco').addClass('changeMenu');
+     $('.tomMenu').addClass('tomMenuResize');
+// $('tomMenu').click(function(){  
+    // $('.tomMenu').width = "200px";
+    // $('.tomMenu').height = "200px";
+});
+});
